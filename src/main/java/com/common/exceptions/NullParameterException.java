f8ec7b6380cf10a83fb6f81282817713e204d009
serge29041996 <sergei29041996@gmail.com>
2018-07-18 09:54:26 +0300

    Update response status for NullParameterException

--- src/main/java/com/common/exceptions/NullParameterException.java
+++ src/main/java/com/common/exceptions/NullParameterException.java
@@ -1,8 +1,12 @@
 package com.common.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Exception where parameters is a null
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NullParameterException extends RuntimeException {
   public NullParameterException(String message) {
     super(message);
