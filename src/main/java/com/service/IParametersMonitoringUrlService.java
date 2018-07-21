0a0090ecc2ce2360a6c9b27e779f94459a3efa4e
serge29041996 <sergei29041996@gmail.com>
2018-07-21 18:03:39 +0300

    Realizations of interface and service for parameters

--- src/main/java/com/service/IParametersMonitoringUrlService.java
+++ src/main/java/com/service/IParametersMonitoringUrlService.java
@@ -1,17 +1,21 @@
 package com.service;
 
 import com.common.ParametersMonitoringUrl;
+import com.common.exceptions.*;
 
 public interface IParametersMonitoringUrlService {
-  ParametersMonitoringUrl getParametersByUrl(String url);
+  ParametersMonitoringUrl getParametersByUrl(String url) throws NotFoundParametersUrlException;
 
-  ParametersMonitoringUrl getParametersById(long id);
+  ParametersMonitoringUrl getParametersById(long id) throws NotFoundParametersUrlException;
 
-  ParametersMonitoringUrl saveParametersUrl(ParametersMonitoringUrl parameters);
+  ParametersMonitoringUrl saveParametersUrl(ParametersMonitoringUrl parametersUrl) throws NullParameterException,
+      ExistingParametersUrlException, CompareTimesException, EqualTimesException;
 
-  void updateParametersUrl(long idUpdatedUrl, ParametersMonitoringUrl newParameters);
+  void updateParametersUrl(long idUpdatedUrl, ParametersMonitoringUrl newParameters) throws
+      NotFoundParametersUrlException, NullParameterException, EqualParametersException, CompareTimesException,
+      EqualTimesException;
 
-  void deleteParametersMonitoringUrl(long id);
+  void deleteParametersMonitoringUrl(long id) throws NotFoundParametersUrlException;
 
   // For testing
   void deleteAll();
--- src/main/java/com/service/ParametersMonitoringUrlService.java
+++ src/main/java/com/service/ParametersMonitoringUrlService.java
@@ -1,46 +1,105 @@
 package com.service;
 
 import com.common.ParametersMonitoringUrl;
+import com.common.exceptions.*;
 import com.dao.ParametersMonitoringUrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
+
 public class ParametersMonitoringUrlService implements IParametersMonitoringUrlService {
+
+  private final ParametersMonitoringUrlRepository parametersUrlRepository;
+
   @Autowired
-  private ParametersMonitoringUrlRepository parametersUrl;
-
-  @Override
-  public ParametersMonitoringUrl getParametersByUrl(String url) {
-    return parametersUrl.findByUrl(url);
+  public ParametersMonitoringUrlService(ParametersMonitoringUrlRepository parametersMonitoringUrlRepository) {
+    this.parametersUrlRepository = parametersMonitoringUrlRepository;
   }
 
   @Override
-  public ParametersMonitoringUrl getParametersById(long id) {
-    return parametersUrl.findById(id);
+  public ParametersMonitoringUrl getParametersByUrl(String url) throws NotFoundParametersUrlException {
+    ParametersMonitoringUrl findByUrlParams = parametersUrlRepository.findByUrl(url);
+    if(findByUrlParams != null) {
+      return findByUrlParams;
+    } else {
+      throw new NotFoundParametersUrlException("Parameters for url " + url + " was not found");
+    }
   }
 
   @Override
-  public ParametersMonitoringUrl saveParametersUrl(ParametersMonitoringUrl parameters) {
-    return null;
+  public ParametersMonitoringUrl getParametersById(long id) throws NotFoundParametersUrlException {
+    ParametersMonitoringUrl findByIdParams = parametersUrlRepository.findById(id);
+    if(findByIdParams != null) {
+      return findByIdParams;
+    } else {
+      throw new NotFoundParametersUrlException("Parameters was not found");
+    }
   }
 
   @Override
-  public void updateParametersUrl(long idUpdatedUrl, ParametersMonitoringUrl newParameters) {
-
+  public ParametersMonitoringUrl saveParametersUrl(ParametersMonitoringUrl parameters) throws
+      NullParameterException, ExistingParametersUrlException, CompareTimesException, EqualTimesException {
+    isNullParameter(parameters);
+    isExistSameParametersForUrl(parameters);
+    compareTimes(parameters);
+    return parametersUrlRepository.save(parameters);
   }
 
   @Override
-  public void deleteParametersMonitoringUrl(long id) {
+  public void updateParametersUrl(long idUpdatedUrl, ParametersMonitoringUrl newParameters) throws
+      NotFoundParametersUrlException, NullParameterException, EqualParametersException, CompareTimesException,
+      EqualTimesException {
+    ParametersMonitoringUrl oldParameters = getParametersById(idUpdatedUrl);
+    isNullParameter(newParameters);
+    isEqualParams(oldParameters, newParameters);
+    compareTimes(newParameters);
+    newParameters.setId(idUpdatedUrl);
+    parametersUrlRepository.save(newParameters);
+  }
 
+  @Override
+  public void deleteParametersMonitoringUrl(long id) throws NotFoundParametersUrlException {
+    ParametersMonitoringUrl paramsForDelete = getParametersById(id);
+    parametersUrlRepository.delete(paramsForDelete);
+  }
+
+
+  private void isNullParameter(ParametersMonitoringUrl parametersUrl) throws NullParameterException {
+    if (parametersUrl == null) {
+      throw new NullParameterException("No value for parameters");
+    }
+  }
+
+  private void isExistSameParametersForUrl(ParametersMonitoringUrl parametersMonitoringUrl)
+      throws ExistingParametersUrlException {
+    ParametersMonitoringUrl foundByUrlParameters = parametersUrlRepository.findByUrl(parametersMonitoringUrl.getUrl());
+    if(foundByUrlParameters != null) {
+      throw new ExistingParametersUrlException(
+          "Parameters for url " + parametersMonitoringUrl.getUrl() + " already exist");
+    }
+  }
+
+  private void isEqualParams(ParametersMonitoringUrl params1, ParametersMonitoringUrl params2)
+      throws EqualParametersException {
+      if (params1.equals(params2)) {
+        throw new EqualParametersException("No changes in value of parameters");
+      }
+  }
+
+  private void compareTimes(ParametersMonitoringUrl parametersUrl) throws CompareTimesException, EqualTimesException {
+    Date beginTime = parametersUrl.getBeginTimeMonitoring();
+    Date endTime = parametersUrl.getEndTimeMonitoring();
+
+    if (endTime.getTime() < beginTime.getTime()) {
+      throw new CompareTimesException("Begin time is bigger than end time");
+    } else if (endTime.getTime() == beginTime.getTime()) {
+      throw new EqualTimesException("Begin and end time are equal");
+    }
   }
 
   //Using for testing
   @Override
   public void deleteAll() {
-
-  }
-
-  private boolean isExistSameParametersForUrl(long id) {
-    ParametersMonitoringUrl parametersMonitoringUrl = getParametersById(id);
-      return parametersMonitoringUrl != null;
+    parametersUrlRepository.deleteAll();
   }
 }
